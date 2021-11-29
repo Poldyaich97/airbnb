357d8a4305ec22815c3b34e5b3aacae32d45fd35
Poldyaich <daniil.poldyaev97@gmail.com>
2021-11-29 13:24:52 +0500

    Added a logo.svg Added a search.svg I wrote the header.

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,10 +1,38 @@
 import "./Header.css";
 import logo from "./logo.svg";
+import search from "./search.svg";
 function Header() {
   return (
     <header className="header">
       <div className="container">
-        <img src={logo} />
+        <div className="header__logo">
+          <img src={logo} />
+        </div>
+        <div className="header__search">
+          <button className="search_btn">
+            <img src={search} alt="search logo" />
+          </button>
+          <input
+            type="search"
+            name="test"
+            id="site-search"
+            placeholder="Try “Miami”"
+          />
+        </div>
+        <div className="header_menu">
+          <a href="#" className="menu_links">
+            Become a host
+          </a>
+          <a href="#" className="menu_links">
+            Help
+          </a>
+          <a href="#" className="menu_links">
+            Sign Up
+          </a>
+          <a href="#" className="menu_links">
+            Log In
+          </a>
+        </div>
       </div>
     </header>
   );
