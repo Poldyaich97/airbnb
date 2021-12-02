017267fe688c00642a95df29d883dba83e9382df
Poldyaich <daniil.poldyaev97@gmail.com>
2021-12-02 13:41:33 +0500

    Fixed

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -5,33 +5,43 @@ function Header() {
   return (
     <header className="header">
       <div className="container">
-        <div className="header__logo">
-          <img src={logo} />
-        </div>
-        <div className="header__search">
-          <button className="search_btn">
-            <img src={search} alt="search logo" />
-          </button>
-          <input
-            type="search"
-            name="test"
-            id="site-search"
-            placeholder="Try “Miami”"
-          />
-        </div>
-        <div className="header_menu">
-          <a href="#" className="menu_links">
-            Become a host
-          </a>
-          <a href="#" className="menu_links">
-            Help
-          </a>
-          <a href="#" className="menu_links">
-            Sign Up
-          </a>
-          <a href="#" className="menu_links">
-            Log In
-          </a>
+        <div className="content">
+          <div className="header__logo">
+            <img src={logo} />
+          </div>
+          <div className="header__search">
+            <div className="header__search_wrapper">
+              <input
+                type="search"
+                name="test"
+                id="site-search"
+                placeholder="Try “Miami”"
+                // className="header__site-search"
+              />
+            </div>
+          </div>
+          <nav className="header__menu">
+            <li>
+              <a href="#" className="header__menu_links">
+                Become a host
+              </a>
+            </li>
+            <li>
+              <a href="#" className="header__menu_links">
+                Help
+              </a>
+            </li>
+            <li>
+              <a href="#" className="header__menu_links">
+                Sign Up
+              </a>
+            </li>
+            <li>
+              <a href="#" className="header__menu_links">
+                Log In
+              </a>
+            </li>
+          </nav>
         </div>
       </div>
     </header>
