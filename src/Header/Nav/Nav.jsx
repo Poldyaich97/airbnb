76fc20dce6f78fef80604b276b64a8196dfe27b3
poldyaich <daniil.poldyaev@Yandex.ru>
2021-12-07 18:00:01 +0500

    Header done

--- src/Header/Nav/Nav.jsx
+++ src/Header/Nav/Nav.jsx
@@ -1,14 +1,15 @@
 import styled from "styled-components";
-
 const StyledNav = styled.nav`
   padding: 27px 0px 28px 189px;
   margin-left: auto;
 `;
 
 const Link = styled.a`
+  font-family: Circular Air;
   font-size: 14px;
   line-height: 24px;
-  margin-right: 30px;
+  font-style: normal;
+  font-weight: normal;
   text-decoration: none;
   color: black;
 `;
@@ -17,23 +18,23 @@ const ListItem = styled.li`
   text-decoration: none;
   list-style: none;
   display: inline-block;
-  margin-right: 30px;
+  margin-right: 8px;
 `;
 
 function Nav() {
   return (
     <StyledNav>
       <ListItem>
-        <Link href="#">Test</Link>
+        <Link href="#">Become a host</Link>
       </ListItem>
       <ListItem>
-        <Link href="#">Test</Link>
+        <Link href="#">Help</Link>
       </ListItem>
       <ListItem>
-        <Link href="#">Test</Link>
+        <Link href="#">Sign Up</Link>
       </ListItem>
       <ListItem>
-        <Link href="#">Test</Link>
+        <Link href="#">Log In</Link>
       </ListItem>
     </StyledNav>
   );
--- src/Header/Search/Search.jsx
+++ src/Header/Search/Search.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import search from "./search.svg";
 
 const Test = styled.div`
   padding: 16px 10px;
@@ -11,17 +12,19 @@ const Wrapper = styled.div`
   border-radius: 4px;
   box-shadow: 0px 2px 4px rgba(72, 72, 72, 0.08);
   position: relative;
+  padding: 12px 40px 12px 0px;
 `;
+
 const Input = styled.input.attrs((props) => ({
   type: "search",
   placeholder: "Try “Miami”",
 }))`
-  width: 100%;
-  height: 100%;
+  width: 350px;
+  height: 24px;
   font-size: 16px;
   line-height: 24px;
   border: none;
-  padding: 12px 191px 12px 53px;
+  padding: 0px 10px 0px 50px;
 `;
 
 export default function Search() {
